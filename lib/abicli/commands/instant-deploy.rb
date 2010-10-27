4e2f5fe51579c4ed49467f4c3a6d03a63a4c475d
Sergio Rubio <sergio@rubio.name>
2010-10-27 15:57:10 +0200

    use env var MEM in run.sh

--- lib/abicli/commands/instant-deploy.rb
+++ lib/abicli/commands/instant-deploy.rb
@@ -196,14 +196,14 @@ if ARGV[0] == 'instant-deploy'
       f.puts "MEM=#{mem}"
       f.puts "TAP=vtap0"
       f.puts ""
-      f.puts "kvm -m #{mem} -drive file=#{File.basename(disk_file)} -net user,hostfwd=tcp:0.0.0.0:#{tomcat_port}-:8080,hostfwd=tcp:0.0.0.0:#{ssh_port}-:22 -net nic -boot order=c > /dev/null"
+      f.puts "kvm -m $MEM -drive file=#{File.basename(disk_file)} -net user,hostfwd=tcp:0.0.0.0:#{tomcat_port}-:8080,hostfwd=tcp:0.0.0.0:#{ssh_port}-:22 -net nic -boot order=c > /dev/null"
       f.puts ""
       f.puts "#"
       f.puts "# Comment the above line and uncomment this to use bridged networking."
       f.puts "# You will need to have a working bridge setup in order to use this."
       f.puts "# Update TAP variable above to fill your needs."
       f.puts "#"
-      f.puts "#kvm -m #{mem} -drive file=#{File.basename(disk_file)} -net tap,ifname=$TAP -net nic -boot order=c > /dev/null 2>&1"
+      f.puts "#kvm -m $MEM -drive file=#{File.basename(disk_file)} -net tap,ifname=$TAP -net nic -boot order=c > /dev/null 2>&1"
     end
     boot_vm :disk_file => disk_file, :cdrom => cdrom, :mem => mem, :tomcat_port => tomcat_port, :ssh_port => ssh_port
   end
